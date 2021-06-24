8019e2d1b8966901e55d0c307a9f7226fa9bc163
devops <devops@example.com>
2021-06-24 05:55:27 +0000

    third push

--- createEc2/Project/ec2.tf
+++
@@ -1,3 +0,0 @@
-module "myweb" {
-  source = "../../modules/"
-}
--- createEc2/Project/provider.tf
+++
@@ -1,6 +0,0 @@
-provider "aws" {
-  access_key   = "AKIAQW4MBWKC4ZCJK4BN"
-  secret_key = "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"
-  region = "region"
-}
-
